32bb0ddf5727777b7eab8adb4c6079007f9008ad
PlattDominic <74417360+PlattDominic@users.noreply.github.com>
2023-05-15 13:57:47 -0400

    Forgot to save before prior commit

--- MinimalJwt/MinimalJwt/Program.cs
+++ MinimalJwt/MinimalJwt/Program.cs
@@ -19,7 +19,25 @@ builder.Services.AddSwaggerGen(options =>
         BearerFormat = "JWT",
         In = ParameterLocation.Header,
         Name = "Authorization",
-        Description = "Bearer Authentication with JWT Token"
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+            {
+
+            }
+        }
     });
 });
 
